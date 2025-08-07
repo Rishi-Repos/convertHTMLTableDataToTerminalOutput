dc09a4d499f482cc23eed58e2f3e4f84cc8d6fc3
Rishi <Rishi.Dhar.82@gmail.com>
2025-08-07 12:31:01 -0400

    completed challenge

--- convertHTMLtableToTerminalOutput.py
+++ convertHTMLtableToTerminalOutput.py
@@ -23,12 +23,15 @@ def print_grid(url):
             longest_y = int(row[2])
         if int(row[0])>longest_x:
             longest_x = int(row[0])
-    # Create sorted list with spaces as default
+    # Create sorted 2Dlist with spaces as default
     sorted = [[' ' for _ in range(longest_x + 1)] for _ in range(longest_y + 1)]
     for row in data:
-        sorted[int(row[0])][int(row[2])] = row[1]
-    sorted.reverse()
+        sorted[int(row[2])][int(row[0])] = row[1]
+    condensed = ['']
     for row in sorted:
+        condensed.append(''.join(row))
+    condensed.reverse()
+    for row in condensed:
         print(row)
 
 url = 'https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub'
